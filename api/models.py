d966922114b508097d3a2618b984c97b8c19a22e
randyjap <randyjap@gmail.com>
2017-02-15 15:57:02 -0800

    Added timzones

--- api/models.py
+++ api/models.py
@@ -4,10 +4,17 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.auth.models import AbstractUser
 
+from django.utils import timezone
+
 class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.first_name, self.last_name
+
 
 class Doctor(models.Model):
     salutation = models.CharField(max_length=20)
@@ -19,13 +26,25 @@ class Doctor(models.Model):
     state = models.CharField(max_length=50)
     lat = models.FloatField()
     lng = models.FloatField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.first_name, self.last_name
 
 class Appointment(models.Model):
     time = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.user.id, self.doctor.id
 
 class Patient(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.first_name, self.last_name
