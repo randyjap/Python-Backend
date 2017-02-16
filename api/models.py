67dcd02954fb23e2d83ca62a0d377ece8dd2c264
randyjap <randyjap@gmail.com>
2017-02-15 16:08:43 -0800

    Custom user

--- api/models.py
+++ api/models.py
@@ -4,13 +4,14 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.auth.models import AbstractUser
 
-from django.utils import timezone
+# from django.contrib.auth import get_user_model
+# User = get_user_model()
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
-    created_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.first_name, self.last_name
@@ -26,7 +27,7 @@ class Doctor(models.Model):
     state = models.CharField(max_length=50)
     lat = models.FloatField()
     lng = models.FloatField()
-    created_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.first_name, self.last_name
@@ -35,7 +36,7 @@ class Appointment(models.Model):
     time = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.id, self.doctor.id
@@ -44,7 +45,7 @@ class Patient(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    created_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.first_name, self.last_name
